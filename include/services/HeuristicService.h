5e0aff156f21f1ef7e71b693b67ef083bb282256
pirabaud <rabaudp@gmail.com>
2025-09-25 16:37:56 +0200

    feat: create heuristic function

---
+++ include/services/HeuristicService.h
@@ -0,0 +1,18 @@
+//
+// Created by pirabaud on 9/25/25.
+//
+
+#ifndef GOMOKU_HEURISTIC_SERVICE_H
+#define GOMOKU_HEURISTIC_SERVICE_H
+#include "Board.h"
+
+
+class HeuristicService {
+public:
+    HeuristicService() = delete;
+
+    static int getHeuristicValue(Board& board);
+};
+
+
+#endif //GOMOKU_HEURISTIC_SERVICE_H
